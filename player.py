90d084b19d07363f06100f92f00aa4c469fb17a3
MAziz-6 <matt.aziz6@gmail.com>
2025-02-04 09:57:41 -0800

    added shot cooldown logic

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
         self.y = y
         super().__init__(self.x, self.y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_clock = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -36,6 +37,9 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if self.shot_clock > 0:
+            self.shot_clock -= dt
+
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
         if keys[pygame.K_d]:
@@ -45,7 +49,10 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(dt*-1)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_clock <= 0:
+                self.shoot()
+                self.shot_clock = PLAYER_SHOOT_COOLDOWN
+        
     
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
